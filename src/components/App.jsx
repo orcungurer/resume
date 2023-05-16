ab23bc36308959502f39605f55f30eda148f5e04
orcungurer <orcungurer@hotmail.com>
2023-05-16 21:38:03 +0300

    beautify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import PersonalInformation from './PersonalInformation';
-import Experience from './Experience';
-import Education from './Education';
-import TechStack from './TechStack';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import PersonalInformation from "./PersonalInformation";
+import Experience from "./Experience";
+import Education from "./Education";
+import TechStack from "./TechStack";
 // import Projects from './Projects';
-import Footer from './Footer';
+import Footer from "./Footer";
 
 function App() {
-    return (
-        <Container fluid>
-            <PersonalInformation />
-            <Experience />
-            <Education />
-            <TechStack />
-            {/* <Projects /> */}
-            <Footer />
-        </Container>
-    );
+  return (
+    <Container fluid>
+      <PersonalInformation />
+      <Experience />
+      <Education />
+      <TechStack />
+      {/* <Projects /> */}
+      <Footer />
+    </Container>
+  );
 }
 
 export default App;
