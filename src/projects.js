f4784862999c9b841d048c00dbdd0d2546473e5f
orcungurer <orcungurer@hotmail.com>
2023-05-18 16:49:14 +0300

    beautify

--- src/projects.js
+++ src/projects.js
@@ -1,22 +1,22 @@
 const projects = [
-    {
-        id: 1,
-        name: "To-Do List",
-        tag: "project-todo",
-        link: "https://orcungurer.github.io/todo/"
-    },
-    {
-        id: 2,
-        name: "Keeper Clone",
-        tag: "project-keeper",
-        link: "https://orcungurer.github.io/keeper/"
-    },
-    {
-        id: 3,
-        name: "Trivia Game",
-        tag: "project-trivia",
-        link: "https://orcungurer.github.io/trivia/"
-    }
+  {
+    id: 1,
+    name: "To-Do List",
+    tag: "project-todo",
+    link: "https://orcungurer.github.io/todo/",
+  },
+  {
+    id: 2,
+    name: "Keeper Clone",
+    tag: "project-keeper",
+    link: "https://orcungurer.github.io/keeper/",
+  },
+  {
+    id: 3,
+    name: "Trivia Game",
+    tag: "project-trivia",
+    link: "https://orcungurer.github.io/trivia/",
+  },
 ];
 
 export default projects;
