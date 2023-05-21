992c5256c6fe2a636c07d6cf8b69a8c733101ad8
orcungurer <orcungurer@hotmail.com>
2023-05-21 22:49:05 +0300

    beautify

--- src/experiences.js
+++ src/experiences.js
@@ -1,50 +1,50 @@
 var experiences = [
-    {
-        id: 4,
-        company: "Rovenma",
-        location: "Ankara, Turkey",
-        title: "Software Engineer, Frontend",
-        startDate: "05/2019",
-        endDate: "01/2023",
-        description: [
-            "Designed and developed web applications from scratch for both crypto devices and smart lockers using the functional components of ReactJS.",
-            "Took over existing smart locker projects; designed and implemented new features.",
-            "Managed app-wide state using React Context. Handled routing in SPAs using React Router.",
-            "Developed applications with a modern and mobile-responsive approach using CSS, SCSS & Bootstrap.",
-            "Worked with both backend and product team members to implement features."
-        ]
-    },
-    {
-        id: 3,
-        company: "Comodo",
-        location: "Ankara, Turkey",
-        title: "DevOps Engineer",
-        startDate: "03/2018",
-        endDate: "02/2019",
-        description: [
-            "Experienced with Linux Based Operating Systems.",
-            "Familiarity with Amazon Web Services using EC2, RDS, VPC, Cognito, CloudWatch.",
-            "Familiarity with ELK and Zabbix monitoring tools and infrastructure components such as Apache/Nginx, Tomcat and Jetty.",
-            "Worked with database systems such as MySQL and MongoDB for both managing & migrating processes and operations.",
-            "Used Jenkins as CI/CD tool, code deployments were done to the related environment by managing and executing bash scripts."
-        ]
-    },
-    {
-        id: 2,
-        company: "Ard Group",
-        location: "Ankara, Turkey",
-        title: "Software Engineer Intern",
-        startDate: "07/2016",
-        endDate: "08/2016",
-    },
-    {
-        id: 1,
-        company: "Turkish Aerospace Industries, Inc. (TAI)",
-        location: "Ankara, Turkey",
-        title: "Software Engineer Intern",
-        startDate: "07/2015",
-        endDate: "08/2015",
-    },
+  {
+    id: 4,
+    company: "Rovenma",
+    location: "Ankara, Turkey",
+    title: "Software Engineer, Frontend",
+    startDate: "05/2019",
+    endDate: "01/2023",
+    description: [
+      "Designed and developed web applications from scratch for both crypto devices and smart lockers using the functional components of ReactJS.",
+      "Took over existing smart locker projects; designed and implemented new features.",
+      "Managed app-wide state using React Context. Handled routing in SPAs using React Router.",
+      "Developed applications with a modern and mobile-responsive approach using CSS, SCSS & Bootstrap.",
+      "Worked with both backend and product team members to implement features.",
+    ],
+  },
+  {
+    id: 3,
+    company: "Comodo",
+    location: "Ankara, Turkey",
+    title: "DevOps Engineer",
+    startDate: "03/2018",
+    endDate: "02/2019",
+    description: [
+      "Experienced with Linux Based Operating Systems.",
+      "Familiarity with Amazon Web Services using EC2, RDS, VPC, Cognito, CloudWatch.",
+      "Familiarity with ELK and Zabbix monitoring tools and infrastructure components such as Apache/Nginx, Tomcat and Jetty.",
+      "Worked with database systems such as MySQL and MongoDB for both managing & migrating processes and operations.",
+      "Used Jenkins as CI/CD tool, code deployments were done to the related environment by managing and executing bash scripts.",
+    ],
+  },
+  {
+    id: 2,
+    company: "Ard Group",
+    location: "Ankara, Turkey",
+    title: "Software Engineer Intern",
+    startDate: "07/2016",
+    endDate: "08/2016",
+  },
+  {
+    id: 1,
+    company: "Turkish Aerospace Industries, Inc. (TAI)",
+    location: "Ankara, Turkey",
+    title: "Software Engineer Intern",
+    startDate: "07/2015",
+    endDate: "08/2015",
+  },
 ];
 
 export default experiences;
